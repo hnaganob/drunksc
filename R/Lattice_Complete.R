d7459a5595263c54731632b91743d133fffb0d45
hnaganob <hnaganob@gmail.com>
2025-07-23 05:08:00 +0900

    refactor: clean up

--- R/Lattice_Complete.R
+++ R/Lattice_Complete.R
@@ -1,53 +1,32 @@
-# Lattice_Complete                                      ---- function ----
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-library(network)
+lattice_complete <- function(n = 5, display_plot = TRUE) {
+  # adjacency matrix ----
+  adj <- matrix(1, ncol = n, nrow = n, dimnames = list(1:n, 1:n))
+  diag(adj) <- 0
 
-# returns:
-#
-# Adj: n nodes adjacency matrix
-# Crd: Adj's circular coordinates 
-#
-#
 
-Lattice_Complete <- function( n = 5 )
-{
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Adjacency Matrix
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  adj <- matrix( 1 , ncol = n , nrow = n )
-  diag( adj ) <- 0
-  
-  Net <- as.network( adj , directed = F )
-  Adj <- as.matrix( Net , matrix.type = "adjacency" )
-  
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Coordinates
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  theta <- seq( 0 , n , 1 ) * 2*pi/n
-  x <- sin( theta[-(n+1)] )
-  y <- cos( theta[-(n+1)] )
-  
-  Crd <- as.data.frame( cbind( x , y ) )
-  
-  plot( Net ,
-        coord = Crd , jitter = F ,
-        displaylabels = T ,
-        label.pos = 1 ,
-        edge.col = "gray" )
-  
-  return( list( Adj = Adj , Crd = Crd ) )
-  
+  # coordinates ----
+  theta <- seq(0, 2 * pi, length.out = n + 1)[-(n + 1)]
+  x <- sin(theta)
+  y <- cos(theta)
+  coord <- data.frame(x = x, y = y)
+
+
+  # network ----
+  net <- as.network(adj, directed = FALSE)
+
+
+  # plot ----
+  if (isTRUE(display_plot)) {
+    par(mai = rep(0, 4))
+    plot(
+      net,
+      coord = coord,
+      jitter = FALSE,
+      displaylabels = TRUE,
+      label.pos = 1,
+      edge.col = "gray"
+    )
+  }
+
+  return(list(adj = adj, coord = coord, net = net))
 }
-
-# Lat_Comp <- Lattice_Comp( 64 )
-# Adj_Comp <- Lat_Comp$Adj
-# Crd_Comp <- Lat_Comp$Crd
-# 
-# plot( as.network( Adj_Comp , directed = F) ,
-#       coord = Crd_Comp , jitter = F ,
-#       edge.col = "gray" ,
-#       displaylabels = T ,
-#       label.pos = 1 )
