d8fe1e422a70b50a2cd5c1695a32d7a45513dd9b
hnaganob <hnaganob@gmail.com>
2025-07-24 02:12:45 +0900

    refactor: clean up

--- R/Lattice_Cube.R
+++ R/Lattice_Cube.R
@@ -1,119 +1,94 @@
-# Lattice_Cube                                          ---- function ----
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-library(network)
+Lattice_Cube <- function(nx = 3, ny = 4, nz = 5, torus = TRUE, display_plot = TRUE) {
+  # adjacency matrix ----
 
-# returns:
-#
-# Adj: nX x nY x nZ nodes adjacency matrix
-# Crd: Adj's cubic coordinates 
-
-
-
-Lattice_Cube <- function( nX = 3 , nY = 4 , nZ = 5 , Torus = T )
-{
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Adjacency Matrix
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  #~~~~~~~~~~~~~~~
+  # ~~~~~~~~~~~~~~~
   # 1d
-  #~~~~~~~~~~~~~~~
-  adj <- matrix( 0 , ncol = nX , nrow = nX )
-  
-  for( i in 1:nX )
+  # ~~~~~~~~~~~~~~~
+  adj <- matrix(0, ncol = nx, nrow = nx)
+
+  for (i in 1:nx)
   {
-    if( i != nX )
-    {
-      adj[ i , i+1 ] <- 1
+    if (i != nx) {
+      adj[i, i + 1] <- 1
     }
     # wrap it
-    if( Torus )
-    {
-      adj[ 1 , nX ] <- 1
+    if (torus) {
+      adj[1, nx] <- 1
     }
   }
-  
-  #~~~~~~~~~~~~~~~
+
+  # ~~~~~~~~~~~~~~~
   # 2d
-  #~~~~~~~~~~~~~~~
-  adj <- kronecker( diag(nY) , adj )
-  
-  for( j in 1:nY )
+  # ~~~~~~~~~~~~~~~
+  adj <- kronecker(diag(ny), adj)
+
+  for (j in 1:ny)
   {
-    for( i in 1:nX )
+    for (i in 1:nx)
     {
-      if( j != nY )
-      {
-        adj[ i + (j-1)*(nX) ,  i + (j)*(nX) ] <- 1
+      if (j != ny) {
+        adj[i + (j - 1) * (nx), i + (j) * (nx)] <- 1
       }
       # wrap around
-      if( Torus )
-      {
-        adj[ i , i + (nY-1)*(nX) ] <- 1
+      if (torus) {
+        adj[i, i + (ny - 1) * (nx)] <- 1
       }
     }
   }
-  
-  #~~~~~~~~~~~~~~~
+
+  # ~~~~~~~~~~~~~~~
   # 3d
-  #~~~~~~~~~~~~~~~
-  adj <- kronecker( diag(nZ) , adj )
+  # ~~~~~~~~~~~~~~~
+  adj <- kronecker(diag(nz), adj)
 
-  for( k in 1:nZ )
+  for (k in 1:nz)
   {
-    for( ij in 1:(nX*nY) )
+    for (ij in 1:(nx * ny))
     {
-      if( k != nZ )
-      {
-        adj[ ij + (k-1)*(nX*nY) , ij + (k)*(nX*nY) ] <- 1
+      if (k != nz) {
+        adj[ij + (k - 1) * (nx * ny), ij + (k) * (nx * ny)] <- 1
       }
       # wrap around
-      if( Torus )
-      {
-        adj[ ij , ij + (nZ-1)*(nX*nY) ] <- 1
+      if (torus) {
+        adj[ij, ij + (nz - 1) * (nx * ny)] <- 1
       }
     }
   }
 
-  Net <- as.network( adj , directed = F )
-  Adj <- as.matrix( Net , matrix.type = "adjacency" )
-  
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Coordinates
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  x <- rep( 1:nX , times=nY*nZ+1e-7 )  # See ?rep
-  y <- rep( 1:nY , each=nX+1e-7 , times=nZ+1e-7 ) 
-  z <- rep( 1:nZ , each=nX*nY+1e-7 ) 
-  
-  xShift <- y * 1/nY
-  yShift <- z * (nZ*2)
-  
-  x <- nY * (x + xShift) * 2
-  y <- -y - yShift
-  # plot( x , y )
-  
-  Crd <- as.data.frame( cbind( x , y ) )
-  # Crd <- Crd - matrix(colMeans(Crd),ncol=2,nrow=dim(Crd)[1],byrow=T)
-  
-  # plot( Net ,
-  #       coord = Crd , jitter = F ,
-  #       displaylabels = T ,
-  #       label.pos = 1 ,
-  #       edge.col = "gray" )
-  
-  return( list( Adj = Adj , Crd = Crd ) )
-  
-}
+  # symmetrize adjacency matrix (undirected)
+  adj <- ((adj + t(adj)) > 0) * 1
 
-# Lat_Cub <- Lattice_Cube( nX = 5 , nY = 4 , nZ = 6 , Torus = T )
-# Adj_Cub <- Lat_Cub$Adj
-# Crd_Cub <- Lat_Cub$Crd
-# 
-# plot( as.network( Adj_Cub , directed = F) ,
-#       coord = Crd_Cub , jitter = F ,
-#       edge.col = "gray" ,
-#       displaylabels = T ,
-#       label.pos = 1 )
+
+  # coordinates ----
+  x <- rep(1:nx, times = ny * nz + 1e-7) # See ?rep
+  y <- rep(1:ny, each = nx + 1e-7, times = nz + 1e-7)
+  z <- rep(1:nz, each = nx * ny + 1e-7)
+
+  x_shift <- y * 1 / ny
+  y_shift <- z * (nz * 2)
+
+  x <- ny * (x + x_shift) * 2
+  y <- -y - y_shift
+
+  coord <- data.frame(x = x, y = y)
+
+
+  # network ----
+  net <- as.network(adj, directed = FALSE)
+
+
+  # plot ----
+  if (isTRUE(display_plot)) {
+    par(mai = rep(0, 4))
+    plot(
+      net,
+      coord = coord,
+      jitter = FALSE,
+      displaylabels = TRUE,
+      label.pos = 1,
+      edge.col = "gray"
+    )
+  }
+
+  return(list(adj = adj, coord = coord, net = net))
+}
